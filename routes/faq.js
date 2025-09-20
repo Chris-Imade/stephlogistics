7cffe2d4b3fe0c8196f7356eb6ceeeb541b17f17
@Chrisdevcode <jephthahimade@gmail.com>
2025-09-20 14:32:33 +0100

    updated the website

--- routes/faq.js
+++ routes/faq.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // GET faq page
-router.get('/faq', (req, res) => {
+router.get('/', (req, res) => {
   res.render('faq', {
     title: 'Frequently Asked Questions',
     path: '/faq',
